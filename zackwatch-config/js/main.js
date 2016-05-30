9e8452a36fb3518fb5c3e72e2047ce1f2e3431fc
daktak <daktaklakpak@eml.cc>
2016-05-30 18:39:47 +1000

    json parse

--- zackwatch-config/js/main.js
+++ zackwatch-config/js/main.js
@@ -28,8 +28,8 @@ function loadOptions() {
     $mypplurl[0].value = localStorage.mypplurl;
     $latitude[0].value = localStorage.latitude;
     $longitude[0].value = localStorage.longitude;
-    $defaultlocOnly[0].checked = localStorage.defaultlocOnly === true;
-    $metric[0].checked = localStorage.metric === true;
+    $defaultlocOnly[0].checked = JSON.parse(localStorage.defaultlocOnly) === false;
+    $metric[0].checked = JSON.parse(localStorage.metric) === true;
     //$vibration[0].checked = localStorage.vibration === true;
   }
 }
